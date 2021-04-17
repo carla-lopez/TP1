0e2708629325757b6103cbcfc07d2bed50bfb39a
Lopez Carla <52410259+carla-lopez@users.noreply.github.com>
2021-04-16 21:29:41 -0300

    Add files via upload

---
+++ TP1/src/Calculo.h
@@ -0,0 +1,18 @@
+
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+float addition(float A,float B);
+
+float substraction(float A,float B);
+
+float division(float A,float B);
+
+float multiplication(float A,float B);
+
+int factorial1(int A);
+
+int factorial2(int B);
+#endif // FUNCIONES_H_INCLUDED
+
+
